25ad1bff1cc1057ffc36ede63395242135c83788
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-10-04 12:26:08 -0500

    finished PUT for updating a valid project

--- data/helpers/projectModelRouter.js
+++ data/helpers/projectModelRouter.js
@@ -18,7 +18,12 @@ router.get('/:id/actions', validateProjectId, (req,res) => {
     PM.getProjectActions(id)
         .then(actions => {
             console.log("actions within GET of /:id/actions", actions)
-            res.status(200).json(actions)
+            if(actions.length == 0){
+                return res.status(200).json({error: `you do not have any actions at the moment for this project`})
+            }
+            else {
+                res.status(200).json(actions)
+            }
         })
         .catch(error => {
             console.log(error);
@@ -44,9 +49,33 @@ router.post('/', validateProjectPost, (req,res) => {
 
 //*************************************PUT   *************************************
 
+router.put('/:id', validateProjectId, (req,res) => {
+    const { id } = req.params;
+    const changes = req.body;
 
+    if(!req.body){
+        return res.status(400).json({error: `must provide a body to UPDATE project ${id}!`});  
+    }
 
-//custom middleware
+    PM.update(id, changes)
+        .then(() => {
+            PM.get(id)
+                .then(project => {
+                    res.status(200).json(project);
+                })
+                .catch(error => {
+                    console.log("error in getting project", error)
+                    res.status(500).json({error: `error in getting the project! `})
+                });
+        })
+        .catch(error => {
+            console.log("error in PUT of /:id", error);
+            res.status(500).json({error: `error in updating project with ID: ${id}`})
+        });
+
+})
+
+//CUSTOM MIDDLEWARE
 
 function validateProjectId(req,res,next) {
     const { id } = req.params;
@@ -67,7 +96,7 @@ function validateProjectPost(req,res,next) {
     const { name } = req.body;
     const { description } = req.body;
 
-    
+
     if(!req.body) {
         return res.status(400).json({error: `must provide a body to create a new project!`});
     }
