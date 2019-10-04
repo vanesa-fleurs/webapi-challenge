a46fec04be190433a525d6c974d852a4e76546a5
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-10-04 13:40:44 -0500

    finally got the POST for adding an action to an existing project

--- data/helpers/actionModelRouter.js
+++ data/helpers/actionModelRouter.js
@@ -35,9 +35,19 @@ router.get('/:id', validateProjectId, (req,res) => {
 
 
 
-//*************************************POST  *************************************
-
-
+//*************************************POST == insert  *************************************
+router.post('/:id', validateProjectId, validateActionPost, (req,res) => {
+    const {id} = req.params;
+    const newAction = req.body;
+    AM.insert(newAction)
+        .then(nAction => {
+            res.status(201).json(nAction);
+        })
+        .catch(error => {
+            console.log("err in ACTION POST lf /:id", err);
+            res.status(500).json({error: `There was an error in adding a new action to the project with ID ${id}`});
+        });
+})
 
 
 //*************************************PUT   *************************************
@@ -64,7 +74,44 @@ function validateProjectId(req,res,next) {
         })
 }
 
+function validateActionPost(req,res,next) {
+    const {project_id} = req.body;
+    const {description} = req.body;
+    const {notes} = req.body;
 
+    //conditions concerning the body
+    if(!req.body) {
+        return res.status(400).json({error: `must provide a body to create a new action!`});
+    }
+    if(!project_id){
+        return res.status(400).json({error: `must provide a PROJECT_ID for a new project!`});
+    }
+    if(!description){
+        return res.status(400).json({error: `must provide a DESCRIPTION for a new project!`});
+    }
+    if(!notes){
+        return res.status(400).json({error: `must provide NOTES for a new project!`});
+    }
+    //further conditions
+    if(description.length > 128){
+        return res.status(400).json({error: `DESCRIPTION cannot be more than 128 characters. Shorten the description, please.`});
+    }
+
+    if(typeof description !== "string"){
+        return res.status(400).json({error: `must provide string for name`});
+    }
+    if(typeof notes !== "string"){
+        return res.status(400).json({error: `must provide string for description`});
+    }
+    if(typeof project_id !== "number"){
+        return res.status(400).json({error: `must provide boolean for project_id`});
+    }
+
+    //fin//
+    req.body = { project_id, description, notes }
+    next();
+
+}
 
 
 module.exports = router;
