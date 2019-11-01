f6163522da5c0ba1bc148c376c0a4d1f5464301f
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-11-01 10:56:51 -0500

    validate Project Post and POST done

--- data/helpers/projectRouter.js
+++ data/helpers/projectRouter.js
@@ -24,6 +24,20 @@ router.get('/:id/actions', validateProjectID, async (req,res) => {
 });
 
 
+router.post('/', validateProjectPOST, async (req,res) => {
+    const newProject = req.body;
+    try{
+        const project = await Project.insert(newProject)
+        if(project){
+            res.status(201).json(project)
+        }
+    }
+    catch(error){
+        res.status(500).json({ message: `There was an error creating that new project`});
+    }
+})
+
+
 
 
 /*                  Custom MiddleWare               */
@@ -43,5 +57,30 @@ async function validateProjectID(req,res, next) {
     catch(error) {
         res.status(500).json({ message: "There was an error validating that Project." });
     }
+}
+
+async function validateProjectPOST(req, res, next) {
+    const { name } = req.body;
+    const { description } = req.body;
+
+    if (Object.keys(req.body).length === 0) {
+        res.status(400).json({ message: "Missing Project data." });
+    } 
+    else if(!name){
+        return res.status(400).json({error: `need a NAME for a new project!`});
+    }
+    
+    else if(!description){
+        return res.status(400).json({error: `need a DESCRIPTION for a new project!`});
+    }
+
+    else if(typeof name !== "string"){
+        return res.status(400).json({error: `need STRING for name`});
+    }
+    else if(typeof description !== "string"){
+        return res.status(400).json({error: `need STRING for description`});
+    }
+    req.body = {name, description}
+    next();
 }
 module.exports = router;
